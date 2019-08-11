fb363c755bef9d984f0efeb087de42fea1030ee8
dohuunghia11 <huunghiado1995@gmail.com>
2019-08-11 07:16:21 +0700

    up

---
+++ src/main/java/com/codegym/controller/StoreController.java
@@ -0,0 +1,22 @@
+package com.codegym.controller;
+
+import com.codegym.model.Store;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/store")
+public class StoreController {
+    @ModelAttribute("store")
+    public Store setupStore(){
+        return new Store();
+    }
+
+    @GetMapping("/show")
+    public ModelAndView showStore (@SessionAttribute("store") Store store){
+        ModelAndView modelAndView = new ModelAndView("/store/show");
+        modelAndView.addObject("store",store);
+        return modelAndView;
+    }
+}
